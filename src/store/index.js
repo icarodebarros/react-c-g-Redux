cbd236ee3d471ff1789d29f97ad33530e946879b
Icaro de Barros <icarodebarros@gmail.com>
2022-10-25 19:09:29 -0300

    Rebuilding store through Redux Persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,43 @@
+import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import counterReducer from './counter';
 import authReducer from './auth';
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
 
-const store = configureStore({
-  reducer: { counter: counterReducer, auth: authReducer },
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  auth: authReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
